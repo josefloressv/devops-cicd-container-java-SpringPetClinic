a273f593df6bccb1a9bc1ed4e01721ab3cb60ac7
José Flores <josefloressv@gmail.com>
2022-04-29 04:37:33 -0600

    fixing context path in tests

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -50,7 +50,7 @@ class PetClinicIntegrationTests {
 
 	@Test
 	void testOwnerDetails() {
-		RestTemplate template = builder.rootUri("http://localhost:" + port).build();
+		RestTemplate template = builder.rootUri("http://localhost:" + port + "/app02").build();
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
